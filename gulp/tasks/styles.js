e74e5415a3c5f4ec2595c8039e6bf400a3461f00
Zak <lysakada@gmail.com>
2017-07-08 18:14:14 +0200

    creation de l'automatisation du refresh mais problem avec mixin

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require ("gulp");
+var postCSS=require('gulp-postcss');
+var cssVariable=require('postcss-simple-vars');
+var nested=require('postcss-nested');
+var cssImport=require('postcss-import');
+var autoprefixer=require('autoprefixer');
+var mixins=require('postcss-mixins');
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postCSS([cssImport,mixins,cssVariable,nested,autoprefixer]))
+    .on('error', function(errorInfo){
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+
+});
